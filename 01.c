541e5f73ff119e2284b609c81f060134dd3e1924
MahanteshPatil5 <mantup506@gmail.com>
2026-01-15 01:06:31 +0530

    Array started

--- 01.c
+++ 01.c
@@ -1,7 +1,32 @@
 #include<stdio.h>
 #include<stdlib.h>
+
+// ARRAY
+// int main(){
+//     printf("Here learn DSA And share :");
+//     printf("\nThis is my first day so from tomorrow onwards i will share more code");
+//     return 0;
+// }
+
+// Problem largest and small
+
 int main(){
-    printf("Here learn DSA And share :");
-    printf("\nThis is my first day so from tomorrow onwards i will share more code");
-    return 0;
+    int arr[5];
+    printf("ENter elemts to arr[5] : ");
+    for(int i=0;i<5;i++){
+        scanf("%d",&arr[i]);
+    }
+
+    int large = arr[0];
+    int small = arr[0];
+    for(int i=1;i<5;i++){
+        if(large < arr[i]){
+            large = arr[i];
+        }
+        if(small > arr[i])
+        {    small = arr[i];
+    }
+}
+printf("\nLarge = %d",large);
+printf("\nsmall = %d",small);
 }
