740bcb3e7d9f13b7f61584129deffdb1208d7bab
MahanteshPatil5 <mantup506@gmail.com>
2026-01-18 21:08:42 +0530

    this is to try ALL programs

---
+++ array/try.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int main(){
+    int arr[5];
+    printf("ENter elemts to arr[5] : ");
+    for(int i=0;i<5;i++){
+        scanf("%d",&arr[i]);
+    }
+    int first = arr[0];
+    int last = arr[5];
+
+    for(int i=0;i<4;i++){
+        if(arr){
+        int temp = first;
+        first = last;
+        last = temp;
+    }
+    printf("\nElements of array in reverse are\n::");
+    for(int i=0;i<5;i++){
+        printf("%d",arr[i]);
+    }
+}
