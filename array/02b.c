eeaabc812a5347023fae261a803fa1536e8e0d7c
MahanteshPatil5 <mantup506@gmail.com>
2026-01-20 23:57:09 +0530

    Reverse of an array not using other array

---
+++ array/02b.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<conio.h>
+
+void rev(int arr[]){
+    int first = 0;
+    int last = 5-1;
+    while(first <last){
+
+        int temp = arr[first];
+        arr[first] = arr[last];
+        arr[last] = temp;
+        first ++;
+        last--;
+    }
+}
+int main(){
+    int arr[5];
+    printf("Enter elemts to arr[5] : ");
+    for(int i=0;i<5;i++){
+        scanf("%d",&arr[i]);
+    }
+    rev(arr);
+
+    printf("\nElements of array in reverse are\n::");
+    for(int i=0;i<5;i++){
+        printf("%d",arr[i]);
+    
+    }
+    return 0;
+}
