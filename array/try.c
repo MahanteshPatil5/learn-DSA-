9881159f783893c4cc26b5d7ecddc4f8382d836c
MahanteshPatil5 <mantup506@gmail.com>
2026-01-20 23:58:28 +0530

    trying

--- array/try.c
+++ array/try.c
@@ -1,23 +1,31 @@
 #include<stdio.h>
 #include<stdlib.h>
+#include<conio.h>
 
+void rev(int arr[]){
+    int first = 0;
+    int last = 5-1;
+    while(first <last){
+
+        int temp = arr[first];
+        arr[first] = arr[last];
+        arr[last] = temp;
+        first ++;
+        last--;
+    }
+}
 int main(){
     int arr[5];
     printf("Enter elemts to arr[5] : ");
     for(int i=0;i<5;i++){
         scanf("%d",&arr[i]);
     }
-    int first = arr[0];
-    int last = arr[5];
+    rev(arr);
 
-    for(int i=0;i<4;i++){
-        if(arr){
-        int temp = first;
-        first = last;
-        last = temp;
-    }}
     printf("\nElements of array in reverse are\n::");
     for(int i=0;i<5;i++){
         printf("%d",arr[i]);
     
-}}
+    }
+    return 0;
+}
