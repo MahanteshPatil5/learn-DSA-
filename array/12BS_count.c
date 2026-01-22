9780fcdb4d3cd06021f06cb61d9f7fda88b74592
MahanteshPatil5 <mantup506@gmail.com>
2026-01-23 02:20:03 +0530

    thi is to find the first and last occurance of a key in the sorted array

--- array/12BS_count.c
+++ array/12BS_count.c
@@ -1,59 +1,70 @@
 // this is used to find how many times the key is in the array
-
-void first(int arr[n],int n,int key){
-    int found = -1,first = 0,last = n-1,mid;
-    while(first<=last){
-        mid = (first+last)/2;
-
-        if(arr[mid] == key){
-            last = mid-1;
-        }
-        else if(key>arr[mid]){
-            last = mid-1;
-        }
-        else {
-            first = mid+1;
-        }
-}}
-void last(int arr[],int n,int key){
-    int found = -1,first = 0,last = n-1,mid;
-    while(first<=last){
-        mid = (first+last)/2;
-
-        if(arr[mid] == key){
-            first = mid+1;
-        }
-        else if(key>arr[mid]){
-            first = mid+1;
-        }
-        else {
-            last = mid-1;
-        }
-}}
 #include<stdio.h>
+
+int firstocc(int arr[],int n,int key){
+    int found = -1,first = 0,last = n-1,mid;
+
+    while(first<=last){
+        mid = (first+last)/2;
+
+        if(arr[mid] == key){
+            found = mid;
+            last = mid-1;
+        }
+        else if(key>arr[mid]){
+            first = mid + 1;
+        }
+        else {
+            last = mid - 1;
+        }
+}   return found;
+}
+
+int lastocc(int arr[],int n,int key){
+    int found = -1,first = 0,last = n-1,mid;
+    while(first<=last){
+        mid = (first+last)/2;
+
+        if(arr[mid] == key){
+            found = mid;
+            first = mid+1;
+        }
+        else if(key>arr[mid]){
+            first = mid+1;
+        }
+        else {
+            last = mid-1;
+        }
+}   
+    return found;
+}
+
 int main(){
     int n,key;
-    printf("Emter number of elmens in array : ");
+    printf("Enter number of elmens in array : ");
     scanf("%d",&n);
+
     int arr[n];
     printf("Enter the elements of array in sorting form : ");
     for(int i = 0 ; i<n;i++){
         scanf("%d",&arr[i]);
     }
-    printf("ENter the key to find in the arr: ");
+    printf("Enter the key to find in the arr: ");
     scanf("%d",&key);
 
 
-    int first = first(arr[n],n,key);
-    int last = last(arr[n],n,key);
+    int first = firstocc(arr,n,key);
+    int last = lastocc(arr,n,key);
 
-    int count = (first -last)-1;
-    printf("The count of key is : %d",count);
-    // if(found != -1 ){
-    //     printf("Key elemts %d  is found at the index: %d",key,found);
-    // }
-    // else{
-    //     printf("Key is not there in arr:::");
-    // }
+    int count;
+    if(first == -1){
+        count =0;
+        printf("Key is not found in the array");}
+    else{
+        count = last - first +1;
+        printf("first occurance is at : %d\n",first);
+        printf("last occurance is at : %d\n",last);
+        printf("the count of key is = %d",count);
+    }
     return 0;
 }
