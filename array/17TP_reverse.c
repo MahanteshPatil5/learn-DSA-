3f2d8db5db3461fba6391e5a8772d4a73565cd11
MahanteshPatil5 <mantup506@gmail.com>
2026-01-25 18:09:47 +0530

    working reverse of array using 2 pointers

--- array/17TP_reverse.c
+++ array/17TP_reverse.c
@@ -1,3 +1,6 @@
+// THis is to find the reverse of a numbr using 3 pointers concept::
+// take first and last and if first<last then swap between arr[first] and arr[last]
+// at last pint the arr
 #include<stdio.h>
 int main(){
     int n,first,last;
