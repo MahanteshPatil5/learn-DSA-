227fd621a271f43a1b0d0f0b0d795da23dc212cc
MahanteshPatil5 <mantup506@gmail.com>
2026-01-26 21:51:38 +0530

    TP remove dublicate elemt

---
+++ array/18TP_rm_dubli.c
@@ -0,0 +1,28 @@
+// This is to remove the dublicate elements from the sorted array
+
+#include<stdio.h>
+int main(){
+    int n,first,last;
+    printf("Enter number of elemts of the array: ");
+    scanf("%d",&n);
+
+    int arr[n];
+    printf("Enter the array Elements : ");
+    for(int i=0;i<n;i++){
+        scanf("%d",&arr[i]);
+    }
+    first=0;last=n-1;
+    while(first <= last){
+        for(int i=0;i<n;i++){
+            if(arr[first] == arr[i]){
+                arr[first] = arr[i];
+                first++;
+            }
+        }
+    }
+    printf("The the elemts of array:");
+    for(int i=0;i<n;i++){
+        // printf();
+    }
+    return 0;
+}
