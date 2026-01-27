9a36c0ae8877b8b328ac44b4328c64a8a9b57c90
MahanteshPatil5 <mantup506@gmail.com>
2026-01-28 01:14:11 +0530

    trying

--- array/20TP_movezero.c
+++ array/20TP_movezero.c
@@ -13,13 +13,16 @@ int main(){
         scanf("%d",&arr[i]);
     }
     int first =0,last=n-1,i=0;
-    while(first<last){
+    for(int i=0;i<last;i++){
        if(arr[i] != 0){
             arr[first] = arr[i];
             first++;
         }
         i++;
-        
+    }
+
+    for(int i= first; i<last;i++){
+        arr[i] = 0;
     }
     printf("\nElements of array ::");
     for(int i=0;i<n;i++){
