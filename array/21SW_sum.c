9cc4868ca459a796f4157c9d9aadf00e866f7a54
MahanteshPatil5 <mantup506@gmail.com>
2026-01-28 21:51:28 +0530

    Slinding window new problem

---
+++ array/21SW_sum.c
@@ -0,0 +1,18 @@
+// this is concept of the subarray like sliding window
+// in this problem i will be solving the max sum of subarray of size k
+
+#include<stdio.h>
+int main(){
+    int n,k;
+    printf("Enter number of elemts of arrya: ");
+    scanf("%d",&n);
+    int arr[n];
+    printf("ENter the elemts of array : ");
+    for(int i =0;i<n;i++){
+        scanf("%d",&arr[i]);
+    }
+
+    printf("Enter the lenght of subarray k : ");
+    scanf("%d",&k);
+    
+}
