ba2cbf2c67d4b3205d96f1296f1dcd00cdc5809e
MahanteshPatil5 <mantup506@gmail.com>
2026-02-05 00:23:47 +0530

    revision of the array concept

---
+++ array/revisearray.c
@@ -0,0 +1,27 @@
+// revision of the concpets whic i have solved till now 
+// question is about find max,min of the given array and find the revrse of the array
+
+#include<stdio.h>
+#include<stdlib.h>
+
+int main(){
+    int arr[5];
+    printf("Enter elemts to arr[5] : ");
+    for(int i=0;i<5;i++){
+        scanf("%d",&arr[i]);
+    }
+
+    int found=0,key,index;
+    printf("\nEnter a key to find in array: ");
+    scanf("%d",&key);
+
+    for(int i = 0 ;i<5; i++){
+        
+    }
+
+    if(found){
+        printf("it is present at :: %d ",index+1);
+    }
+    else{printf("It is not possive");
+}
+}
