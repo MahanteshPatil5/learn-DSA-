46ef1e13f977a7c643fdfbe6a0a641d196960527
MahanteshPatil5 <mantup506@gmail.com>
2026-02-07 13:20:11 +0530

    DSA exam file

---
+++ DSA exam/stack.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#define max 10
+int top,stac[max];
+int main(){
+    int n;
+    printf("Enter the number of elemsts of stack : ");
+    scanf("%d",&n);
+    printf("Enter the elements of stack max 10: ");
+    for(int i=0;i<n;i++){
+        scanf("%d",&stac[i]);
+    }
+    printf("");
+
+    
+}
