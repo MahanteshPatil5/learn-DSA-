5e8005becdf7a827a2e7380faa6c301471504fa4
MahanteshPatil5 <mantup506@gmail.com>
2026-02-09 16:34:56 +0530

    SLL insert at front

--- DSAexam/SLL.c
+++ DSAexam/SLL.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include<stdlib.h>
 
 int value; 
 struct node
@@ -10,9 +11,17 @@ struct node *head = NULL;
 void insertF(int value){
     struct node *temp;
     temp = (struct node*)malloc(sizeof(struct node));
-
+    temp->key = value;
+    if(head == NULL){
+        head = temp;
+        temp->next = NULL;
+    }
+    else{
+    temp->next = head;
+    return head;}
 }
 int main(){
     printf("Enter element to insert at front of SLL : ");
     scanf("%d",&value);
+    insertF(value);
 }
