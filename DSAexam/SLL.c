6ec38e809a6695a137f58b8f238d1a223aa21f38
MahanteshPatil5 <mantup506@gmail.com>
2026-02-11 23:54:38 +0530

    working on it

--- DSAexam/SLL.c
+++ DSAexam/SLL.c
@@ -19,29 +19,48 @@ void insertF(int value){
     }
     else{
     temp->next = head;
+    head = temp;
     return head;}
 }
 void disp(){
     struct node *temp;
-    temp = (struct node*)malloc(sizeof(struct node));
     temp = head;
     if(head == NULL){
         printf("The head is empty  ");
     }
     
     else{
-        while(temp->next != NULL){
+        while(temp != NULL){
             printf(" the %d ",temp->key);
-            temp = temp->key;
+            temp = temp->next;
         }
     }
 }
+void insertE()
+{
+    int value;
+    printf("ENter the value to enter @ last of ll :: ");
+    scanf("%d",&value);
+    struct node *temp;
+    temp = (struct node*)malloc(sizeof(struct node));
+    temp->key;
+}
 int main(){
+    int ch;
     printf("\nEnter element to insert at front of SLL : ");
     scanf("%d",&value);
     while(1){
-        insertF(value);
-        disp();
+        printf("Enter your choice : ");
+        scanf("%d",&ch);
+        switch (ch)
+        {
+        case 1:
+            insertF(value);
+            break;
+        
+        default:
+            break;
+        }
     }
     return 0;
 }
