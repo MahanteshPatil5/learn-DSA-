a64b1dadf2c015a0cfbd06ff3ba8c94f5e8cd4dc
MahanteshPatil5 <mantup506@gmail.com>
2026-02-12 00:54:56 +0530

    woking SLL operation

--- DSAexam/SLL.c
+++ DSAexam/SLL.c
@@ -1,15 +1,16 @@
 #include <stdio.h>
 #include<stdlib.h>
-
-int value; 
-
 struct node
 {
   int key;
   struct node *next;  
 };
 struct node *head = NULL;
-void insertF(int value){
+
+void insertF(){
+    int value;
+    printf("Enter the value to enter @ last of ll :: ");
+    scanf("%d",&value);
     struct node *temp;
     temp = (struct node*)malloc(sizeof(struct node));
     temp->key = value;
@@ -22,6 +23,60 @@ void insertF(int value){
     head = temp;
     return head;}
 }
+
+void insertE()
+{
+    int value;
+    printf("ENter the value to enter @ last of ll :: ");
+    scanf("%d",&value);
+    struct node *temp,*p;
+    temp = (struct node*)malloc(sizeof(struct node));
+    temp->key = value;
+    temp->next = NULL;
+    p = head;
+    if(head == NULL){
+        head = temp;
+        temp->next = NULL;
+    }
+    else{
+        while(p->next != NULL){
+            p = p->next; 
+        }
+        p-> next = temp;
+    }
+}
+
+void delF(){
+    struct node *temp;
+    if(head == NULL){
+        printf("There is no nodes in the LL ");
+    }
+    else{
+        temp = head;
+        head = head->next;
+        printf("The deleted node is :: %d",temp->key);
+        free(temp);
+    }
+}
+
+void delE(){
+    struct node *p,*c;
+    if(head == NULL){
+        printf("There is no nodes in the LL ");
+    }
+    else{
+        c = head;
+        while(c->next != NULL){
+            p = c;
+            c = c->next;
+        }
+        head = p;
+        
+        printf("The deleted node is :: %d",c->key);
+        free(c);
+    }
+}
+
 void disp(){
     struct node *temp;
     temp = head;
@@ -36,29 +91,35 @@ void disp(){
         }
     }
 }
-void insertE()
-{
-    int value;
-    printf("ENter the value to enter @ last of ll :: ");
-    scanf("%d",&value);
-    struct node *temp;
-    temp = (struct node*)malloc(sizeof(struct node));
-    temp->key;
-}
 int main(){
     int ch;
-    printf("\nEnter element to insert at front of SLL : ");
-    scanf("%d",&value);
     while(1){
-        printf("Enter your choice : ");
+        printf("\n1.Insert Front 2.Insert End 3.Delete Front 4.Delete End 5.display 6.Exit");
+        printf("\nEnter your choice : ");
         scanf("%d",&ch);
         switch (ch)
         {
         case 1:
-            insertF(value);
+            insertF();
             break;
-        
+        case 2:
+            insertE();
+            break;
+        case 3:
+            delF();
+            break;
+        case 4:
+            delE();
+            break;
+        case 5:
+        disp();
+        break;
+
+        case 6:
+        return 0;
+
         default:
+        printf("\n Enter the proper choice : ");
             break;
         }
     }
