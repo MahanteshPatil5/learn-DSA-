97da5f63ca628dfcb7968c0c48af8386fc0449ea
MahanteshPatil5 <mantup506@gmail.com>
2026-02-12 14:55:26 +0530

    using SW find the target

---
+++ array/22SW_smallsum.c
@@ -0,0 +1,18 @@
+// this program is to use slidning window problem
+// Find the smallest subarray sum > target
+#include<stdio.h>
+int main(){
+    int n,target;
+    printf("Enter the number eements in the array :: ");
+    scanf(" %d ",&n);
+    int arr[n],sum;
+    printf("Enter the Elements of array :: ");
+    for(int i=0;i<n;i++){
+        scanf("%d",&arr[i]);
+    }
+    printf("Enter the Target value :: ");
+    scanf("%d",&target);
+
+    // while()
+    return 0;
+}
