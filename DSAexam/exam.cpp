67b4921841ab1d74f13862e4c23f8a67e5f046da
MahanteshPatil5 <mantup506@gmail.com>
2026-02-14 22:22:01 +0530

    to find max and min

--- DSAexam/exam.cpp
+++ DSAexam/exam.cpp
@@ -1,5 +1,14 @@
 #include<iostream>
 using namespace std;
+
 int main(){
-    cout<<"Hello wolrd";
+    int n;
+    cout<<"Enter the value for n : ";
+    cin>>n;
+    int arr[n];
+    cout<<"Enter the values for array : ";
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+    }
+    int max=arr[0];
 }
