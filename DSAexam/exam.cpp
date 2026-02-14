8832f50afd2d72de0ba2f8a6f242a4cbaa21daed
MahanteshPatil5 <mantup506@gmail.com>
2026-02-14 22:28:44 +0530

    wokiing finding  max and min of array

--- DSAexam/exam.cpp
+++ DSAexam/exam.cpp
@@ -10,5 +10,15 @@ int main(){
     for(int i=0;i<n;i++){
         cin>>arr[i];
     }
-    int max=arr[0];
+    int max=arr[0],min=arr[0];
+    for(int i=0;i<n;i++){
+        if(max<arr[i]){
+            max = arr[i];
+        }
+        if(min > arr[i]){
+            min = arr[i];
+        }
+    }
+    cout<<"MAX :: "<<max<<endl;
+    cout<<"min :: "<<min;
 }
