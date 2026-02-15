81ea093c65565fdd12ef73e64fae4d1f06e061d1
MahanteshPatil5 <mantup506@gmail.com>
2026-02-15 11:23:31 +0530

    find large of e number

--- DSAexam/exam2.cpp
+++ DSAexam/exam2.cpp
@@ -1,15 +1,39 @@
 #include<iostream>
 using namespace std;
 
-class students{
+// use of Class and object
+// class students{
+//     public:
+//     int rollno;
+//     void show(){
+//         cout<<"R N : "<<rollno;
+//     }
+// };
+// int main(){
+//     students ClassA[10];
+//     ClassA[1].rollno = 10;
+//     ClassA[1].show();
+// }
+
+class A{
     public:
-    int rollno;
-    void show(){
-        cout<<"R N : "<<rollno;
+    int a,b,c;
+    A(int x,int y,int z){
+        a=x;
+        b=y;
+        c=z;
+        if(a>b && a>c){
+            cout<<"large is :: "<<a;
+        }
+        else if(b>a && b>c){
+            cout<<"large is :: "<<b;
+        }
+        else{
+            cout<<"large is :: "<<c;
+        }
     }
 };
+
 int main(){
-    students ClassA[10];
-    ClassA[1].rollno = 10;
-    ClassA[1].show();
+    A a(10,20,30);
 }
