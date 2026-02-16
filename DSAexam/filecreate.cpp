c97797568037341d67617fd80879884a5e1d6447
MahanteshPatil5 <mantup506@gmail.com>
2026-02-16 11:36:22 +0530

    this is file operation file

---
+++ DSAexam/filecreate.cpp
@@ -0,0 +1,24 @@
+#include<fstream>
+#include<iostream>
+using namespace std;
+int main(){
+    fstream file;
+    file.open("sample.txt",ios::out);
+    if(!file){
+        cout<<"file not created ";
+    }
+    else{
+        cout<<" file created ";
+    }
+    file<<"thi is file content ";
+    file<<"created and written this in it ";
+
+    file.close();
+
+    file.open("sample.txt", ios::in);
+    cout<<"file content is :: ";
+    char ch;
+    while(file.get(ch)){
+        cout<<ch;
+    }
+}
