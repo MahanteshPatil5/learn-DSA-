d77fa003b447a913e59bd3f03148d4f27b6d2abb
MahanteshPatil5 <mantup506@gmail.com>
2026-02-17 00:38:21 +0530

    Array out of bound error handle

--- DSAexam/exception.cpp
+++ DSAexam/exception.cpp
@@ -1,25 +1,43 @@
 #include<fstream>
 #include<iostream>
 using namespace std;
-int divid(int a,int b){
-    if(b==0){
-        throw(b);
-    }
-    else{
-        return (a/b);
-    }
-}
+// int divid(int a,int b){
+//     if(b==0){
+//         throw(b);
+//     }
+//     else{
+//         return (a/b);
+//     }
+// }
 
+// int main(){
+//     int x,y;
+//     cout<<"Eter value for x,y :: ";
+//     cin>>x>>y;
+
+//     try{
+//         divid(x,y);
+//     }
+//     catch(int num){
+//         cout<<"Error in divizion by zero";
+//     }
+//     return 0;
+// }
+
+// Array out of bound
 int main(){
-    int x,y;
-    cout<<"Eter value for x,y :: ";
-    cin>>x>>y;
+    int arr[5];
+    int ind;
+    cout<<"ENter the elements of the array arr[5] ";
+    for(int ind=0;ind<5;ind++){
+    cin>>arr[ind];}
 
+    cout<<"ENter the array"
     try{
-        divid(x,y);
+    if(ind<0 && ind>5){
+        throw(ind);
+    }}
+    catch(int){
+        cout<<"Error Array out of bound  ";
     }
-    catch(int num){
-        cout<<"Error in divizion by zero";
-    }
-    return 0;
 }
