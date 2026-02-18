5d96b158beef6cb570da767a032e6e8fd82f5182
MahanteshPatil5 <mantup506@gmail.com>
2026-02-18 23:58:25 +0530

    student details using class

--- DSAexam/student.cpp
+++ DSAexam/student.cpp
@@ -1,6 +1,7 @@
 #include<iostream>
 using namespace std;
 class student{
+    public:
     char name[20];
     int roll;
     float pers;
@@ -9,15 +10,16 @@ class student{
 int main(){
     cout<<"Program to print the student details by taking from the teacher ";
     int n;
-    cout<<"Enter the number of students : ";
+    cout<<"\nEnter the number of students : ";
     cin>>n;
-    student std[n];
+    student stud[n];
     
-    cout<<"Enter the student Details :(name roll number and persentage )";
+    cout<<"\nEnter the student Details :(name roll number and persentage )";
     for(int i=0;i<n;i++){
-        cout<<"Enter data od std"<<(i+1);
-        cin>>std[i].name[20];
-        cin>>std[i].roll;
-        cin>>std[i].pers;
+        cout<<"\nEnter data of std"<<(i+1);
+        cin>>stud[i].name;
+        cin>>stud[i].roll;
+        cin>>stud[i].pers;
     }
+    
 }
