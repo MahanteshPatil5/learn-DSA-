d01009a04188d494d748d396c130d079c42cf637
MahanteshPatil5 <mantup506@gmail.com>
2026-02-19 00:27:11 +0530

    tring on it

--- DSAexam/student.cpp
+++ DSAexam/student.cpp
@@ -1,4 +1,5 @@
 #include<iostream>
+#include<fstream>
 using namespace std;
 class student{
     public:
@@ -13,13 +14,29 @@ int main(){
     cout<<"\nEnter the number of students : ";
     cin>>n;
     student stud[n];
-    
+    fstream myfile;
+    myfile.open("stdent.txt",ios::out);
+    if(!myfile){
+        cout<<"\nFile is not created::";
+    }
+    else{
     cout<<"\nEnter the student Details :(name roll number and persentage )";
     for(int i=0;i<n;i++){
         cout<<"\nEnter data of std"<<(i+1);
         cin>>stud[i].name;
         cin>>stud[i].roll;
         cin>>stud[i].pers;
+        myfile<<stud[i].name;
+        myfile<<stud[i].roll;
+        myfile<<stud[i].pers;
+    }}
+    cout<<"\nEnter a roll number to find the student: ";
+    int roll;
+    cin>>roll;
+    for(int i=0;i<n;i++){
+    if(roll == stud[i].roll){
+        cout<<"name: "<<stud[i].name<<"\nroll number : "<<stud[i].roll<<"\npercentage : "<<stud[i].pers;
     }
-    
+    }
+cout<<"\nNoo student with that roll :: ";
 }
