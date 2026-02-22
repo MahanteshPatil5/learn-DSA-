db5421942948360b8dc675032adeaef304f8ce7f
MahanteshPatil5 <mantup506@gmail.com>
2026-02-22 23:16:36 +0530

    This is to find the number of subarray with sum is target

---
+++ array/24SW_countSBA.c
@@ -0,0 +1,23 @@
+// this program will find the number of subarray whose sum is given target
+// to find how many subarray with sum == target
+
+#include<stdio.h>
+int main(){
+    int n;
+    printf("ENter the number of elemsts of the array : ");
+    scanf("%d",&n);
+    int arr[n],target;
+    printf("Enter the elements of the array :: ");
+    for(int i=0;i<n;i++){
+        scanf("%d",&arr[i]);
+    }
+    printf("ENter the target :: ");
+    scanf("%d",&target);
+
+    int sum=0,first=0;
+
+    for(int last=0;last<n;last++){
+        sum = sum+arr[last];
+    }
+
+}
