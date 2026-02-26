cad08df54641104231fca048da71d90d843ee18c
MahanteshPatil5 <mantup506@gmail.com>
2026-02-26 23:26:01 +0530

    LL insert fromt

--- LinkedList/01.c
+++ LinkedList/01.c
@@ -1,4 +1,7 @@
 #include<stdio.h>
+
+int val;
+
 struct node{
     int data;
     struct node *next;
@@ -6,6 +9,34 @@ struct node{
 
 struct node *head = NULL;
 
+
+void insertFront(val){
+    struct node *new = (int*)malloc(sizeof(struct node)) ;
+
+    new->data = val;
+    new->next = head;
+    head = new;
+}
+
+void display(){
+    if(head == NULL){
+        printf("the haed isnull so not able to display ");
+        return;
+    }
+    else{
+        while (head->next != NULL)
+        {
+            printf(" %d ",head->data);
+            head = head->next;
+        }
+        
+    }
+}
+
 int main(){
+    printf("Enter the Element to enter to the LL : ");
+    scanf("%d",&val);
+    insertFront(val);
+    display();
     printf("HELLO WORLD");
 }
